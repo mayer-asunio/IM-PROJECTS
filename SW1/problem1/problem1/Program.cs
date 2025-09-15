d258daea6476e5d2b028df396854cb77b96dabd9
unknown <abellamechaela@gmail.com>
2025-09-15 22:07:14 +0800

    Upload

---
+++ SW1/problem1/problem1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace problem1
+{
+        class Program
+        {
+            static string PrimeOrComposite(int n)
+            {
+                if (n <= 1) return "Composite"; 
+                for (int i = 2; i <= Math.Sqrt(n); i++)
+                {
+                    if (n % i == 0)
+                        return "Composite";
+                }
+                return "Prime";
+            }
+
+            static void Main(string[] args)
+            {
+                Console.Write("Enter numbers (separated by space): ");
+                string[] input = Console.ReadLine().Split();
+
+                for (int i = 0; i < input.Length; i++)
+                {
+                    int num = int.Parse(input[i]);
+                    Console.WriteLine($"{i + 1}. {num,-5} {PrimeOrComposite(num)}");
+                }
+
+                Console.ReadKey();
+            }
+        }
+    }
---
+++ SW1/problem2/problem2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace problem2
+{
+  
+
+    class Program
+    {
+        static (double peso, double yen) ConvertCurrency(double dollar)
+        {
+            double peso = dollar * 57.17;
+            double yen = dollar * 146.67;
+            return (peso, yen);
+        }
+
+        static void Main()
+        {
+            Console.Write("Enter currency in ($): ");
+            string[] values = Console.ReadLine().Split(',');
+
+            Console.WriteLine("\nDollar($)\tPhil Peso(P)\tJpn Yen(Y)");
+            foreach (string v in values)
+            {
+                double dollar = double.Parse(v);
+                var (peso, yen) = ConvertCurrency(dollar);
+                Console.WriteLine($"{dollar}\t\t{peso:F2}\t\t{yen:F2}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
